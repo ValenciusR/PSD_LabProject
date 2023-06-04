774303081d1dab33b89d3b16c8dbb69ac0c2dc07
ValenciusR <valencius.rianto25@gmail.com>
2023-06-04 17:09:23 +0700

    Manage, Insert, Delete Ramen Done (kurang validasi)

---
+++ RAAMEN/RAAMEN/Handler/RamenHandler.cs
@@ -0,0 +1,44 @@
+﻿using RAAMEN.Factory;
+using RAAMEN.Model;
+using RAAMEN.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RAAMEN.Handler
+{
+    public class RamenHandler
+    {
+        // insert
+        public static void insertRamen(int meatId, string name, string broth, int price)
+        {
+            Raman ramen = RamenFactory.createRamen(meatId, name, broth, price);
+            RamenRepository.insertRamen(ramen);
+        }
+
+        // update
+        public static void updateRamen(int id,int meatId, string name, string broth, int price)
+        {
+            RamenRepository.updateRamen(id, meatId, name, broth, price);
+        }
+
+        // delete 
+        public static void deleteRamen(int id)
+        {
+            RamenRepository.deleteRamen(id);
+        }
+
+        // get ramen by id
+        public static Raman getRamenById(int id)
+        {
+            return RamenRepository.getRamenById(id);
+        }
+
+        // get all ramen
+        public static List<Raman> getAllRamen()
+        {
+            return RamenRepository.getAllRamen();
+        }
+    }
+}
---
+++ RAAMEN/RAAMEN/View/ManageRamen.aspx.cs
@@ -0,0 +1,39 @@
+﻿using RAAMEN.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RAAMEN.View
+{
+    public partial class ManageRamen : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ramenGV.DataSource = RamenController.getAllRamen();
+            ramenGV.DataBind();
+        }
+
+        protected void ramenGV_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridViewRow row = ramenGV.Rows[e.NewEditIndex];
+            int id = int.Parse(row.Cells[1].Text);
+            Response.Redirect("~/View/UpdateRamen.aspx?id=" + id);
+        }
+
+        protected void AddBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/View/InsertRamen.aspx");
+        }
+
+        protected void ramenGV_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            GridViewRow row = ramenGV.Rows[e.RowIndex];
+            int id = int.Parse(row.Cells[1].Text);
+            RamenController.deleteRamen(id);
+            Response.Redirect("/View/ManageRamen.aspx");
+        }
+    }
+}
