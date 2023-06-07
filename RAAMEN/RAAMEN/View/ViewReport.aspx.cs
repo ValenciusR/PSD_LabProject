fa093362ea4ec2be8b2c12007b125389c1b4c0ee
Valencius Rianto <127145309+ValenciusR@users.noreply.github.com>
2023-06-08 01:11:04 +0700

    Crystal Report Tinggal cara show price

---
+++ RAAMEN/RAAMEN/View/ViewReport.aspx.cs
@@ -0,0 +1,59 @@
+﻿
+using RAAMEN.Model;
+using RAAMEN.Report;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RAAMEN.View
+{
+    public partial class ViewReport : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CrystalReport report = new CrystalReport();
+            CrystalReportViewer.ReportSource = report;
+
+            DatabaseEntities db = new DatabaseEntities();
+            DataSet data = getData((from header in db.Headers select header).ToList());
+
+            report.SetDataSource(data);
+
+        }
+
+        private DataSet getData(List<Header> headers)
+        {
+            ReportDocument rd = new ReportDocument();
+            
+
+            DataSet data = new DataSet();
+            var headerTable = data.Header;
+            var detailTable = data.Detail;
+            
+            foreach(Header h in headers)
+            {
+                var headerRow = headerTable.NewRow();
+                headerRow["Id"] = h.Id;
+                headerRow["CustomerId"] = h.CustomerId;
+                headerRow["StaffId"] = h.StaffId;
+                headerRow["Date"] = h.Date;
+                headerRow["Status"] = h.Status;
+                headerTable.Rows.Add(headerRow);
+                foreach(Detail d in h.Details)
+                {
+                    var detailRow = detailTable.NewRow();
+                    detailRow["HeaderId"] = d.HeaderId;
+                    detailRow["RamenId"] = d.RamenId;
+                    detailRow["Quantity"] = d.Quantity;
+                    detailTable.Rows.Add(detailRow);
+                    ((CrystalDecisions.CrystalReports.Engine.TextObject)rd.ReportDefinition.ReportObjects["UnboundString1"]).Text = "12";
+                }
+            }
+            return data;
+        }
+    }
+}
